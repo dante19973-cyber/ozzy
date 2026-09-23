797297e0c2edd89667751731da364b868a7fa585
dante19973-cyber <dante19973@gmail.com>
2026-09-23 15:15:49 +0200

    update1

--- src/Album.jsx
+++ src/Album.jsx
@@ -1,12 +1,12 @@
-function Album ({name, year, image,}){
+function Album ({name, year, image,album}){
     return(
         <section className="album">
             <h1>{name}</h1>
-            <img src={image} alt={name} />
+            <img src={image} alt={name}/>
             <h2>{year}</h2>
             <iframe data-testid="embed-iframe"
-                    src="https://open.spotify.com/embed/album/7LGVdC9fFwgWYaIrZwsSv6?utm_source=generator&si=5d1955c2798f43eb"
-                    width="100%" height="352" frameBorder="0" allowFullScreen=""
+                    src={album}
+                    width="100%" height="152" frameBorder="0" allowFullScreen=""
                     allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
                     loading="lazy"></iframe>
         </section>
--- src/AlbumO.jsx
+++ src/AlbumO.jsx
@@ -1,4 +1,68 @@
+import Album from './Album.jsx'
 function AlbumO(){
-
+    return(
+        <section className="albumO">
+            <Album name="Blizzard of Ozz"
+                   year="1981"
+                   image="https://m.media-amazon.com/images/I/51G-r0tMSYL._UF1000,1000_QL80_.jpg"
+                   album="https://open.spotify.com/embed/album/3OmnatFgQ8o1DlYLp9EpNJ?utm_source=generator&si=5efa8d7002724de5"
+            />
+            <Album name="Diary of a Madman"
+                   year="1980"
+                   image="https://m.media-amazon.com/images/I/51G-r0tMSYL._UF1000,1000_QL80_.jpg"
+                   album="https://open.spotify.com/embed/album/4qUMByJ3Pk94BFnCmGaUPS?utm_source=generator&si=785d7a2d4a694f93"
+            />
+            <Album name="Bark at the Moon"
+                   year="1983"
+                   image="https://m.media-amazon.com/images/I/51G-r0tMSYL._UF1000,1000_QL80_.jpg"
+                   album="https://open.spotify.com/embed/track/2E7W1X4maFFcjHrVrFA7Vs?utm_source=generator&si=e55613732ec64032"
+            />
+            <Album name="The Ultimate Sin"
+                   year="1986"
+                   image="https://m.media-amazon.com/images/I/51G-r0tMSYL._UF1000,1000_QL80_.jpg"
+                   album="https://open.spotify.com/embed/album/2IPIumpbhrtBvjyzIgGE9j?utm_source=generator&si=75fbc3344f1647a8"
+            />
+            <Album name="No Rest for the Wicked"
+                   year="1988"
+                   image="https://m.media-amazon.com/images/I/51G-r0tMSYL._UF1000,1000_QL80_.jpg"
+                   album="https://open.spotify.com/embed/album/0gkILbwcOoXEb8D4aRptez?utm_source=generator&si=4b740eebe6f7417b"
+            />
+            <Album name="No More Tears"
+                   year="1991"
+                   image="https://m.media-amazon.com/images/I/51G-r0tMSYL._UF1000,1000_QL80_.jpg"
+                   album="https://open.spotify.com/embed/album/6eh82ojicL8RSJF7GkYTh7?utm_source=generator&si=6cea66d0512e49c0"
+            />
+            <Album name="Ozzmosis"
+                   year="1995"
+                   image="https://m.media-amazon.com/images/I/51G-r0tMSYL._UF1000,1000_QL80_.jpg"
+                   album="https://open.spotify.com/embed/album/5m8Fj0thHTE4CBGJTdF6s9?utm_source=generator&si=045b3c9bb1c04ff4"
+            />
+            <Album name="Down to Earth"
+                   year="2001"
+                   image="https://m.media-amazon.com/images/I/51G-r0tMSYL._UF1000,1000_QL80_.jpg"
+                   album="https://open.spotify.com/embed/album/35FSH5VP6Om7kBFtGn25wm?utm_source=generator&si=bae3157472524010"
+            />
+            <Album name="Black Rain"
+                   year="2007"
+                   image="https://m.media-amazon.com/images/I/51G-r0tMSYL._UF1000,1000_QL80_.jpg"
+                   album="https://open.spotify.com/embed/track/1NXguUURlFigDpGEcdKVUT?utm_source=generator&si=33d2747341434539"
+            />
+            <Album name="Scream"
+                   year="2010"
+                   image="https://m.media-amazon.com/images/I/51G-r0tMSYL._UF1000,1000_QL80_.jpg"
+                   album="https://open.spotify.com/embed/album/72OTYgFKqc6ALaGfXZQUWM?utm_source=generator&si=8b95a7e6fbb0473c"
+            />
+            <Album name="Ordinary Man"
+                   year="2020"
+                   image="https://m.media-amazon.com/images/I/51G-r0tMSYL._UF1000,1000_QL80_.jpg"
+                   album="https://open.spotify.com/embed/album/2x2cG56QicVfymWnRF0Nmj?utm_source=generator&si=e20342db89f0455f"
+            />
+            <Album name="Patient Number 9"
+                   year="2022"
+                   image="https://m.media-amazon.com/images/I/51G-r0tMSYL._UF1000,1000_QL80_.jpg"
+                   album="https://open.spotify.com/embed/album/4u1LXToDGOdhDDf4c9zeqb?utm_source=generator&si=83aee9f5df454607"
+            />
+        </section>
+    )
 }
 export default AlbumO
